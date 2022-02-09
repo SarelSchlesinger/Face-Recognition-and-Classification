3e3a97fb41ab36191c95d092044e7ff03cb60bcd
israel schlesinger <sarelsch@outlook.co.il>
2022-02-09 22:47:13 +0200

    .

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import numpy as np
 import tensorflow as tf
 import cv2
-from CutPic import *
+from PIL import Image
+from autocrop import Cropper
 from Quality_Analysis import *
-
 
 modelPath = "model2.tflite"
 interpreter = tf.lite.Interpreter(modelPath)
@@ -12,11 +12,23 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 
+def CutPic(path):
+    cropper = Cropper(width=300, height=300, face_percent=90)
+    try:
+        cropped_array = cropper.crop(path)
+        cropped_image = Image.fromarray(cropped_array)
+        cropped_image.save('cropped.jpg')
+        return 'cropped.jpg'
+    except:
+        return path
+
+
 def process(image):
     shape_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image_data = cv2.resize(shape_image, (300, 300))
     image_data = image_data[np.newaxis, ...].astype(np.uint8)
     return image_data
+
 
 def imageProcessing(imagePath):
     new_image = CutPic(imagePath)
@@ -34,10 +46,9 @@ def imageProcessing(imagePath):
     output_dict = dict()
     labels = ['blur', 'dark', 'mask', 'good', 'pose', 'rotate', 'sunglasses']
     for index, value in enumerate(predictions_list):
-        output_dict[labels[index]] = value*100
-
-    print(output_dict)
+        output_dict[labels[index]] = value * 100
 
     return Quality_Analysis(output_dict)
+
 
 print(imageProcessing(r"C:\Users\User\Pictures\Camera Roll\WIN_20220209_20_58_06_Pro.jpg"))
