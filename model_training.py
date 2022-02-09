25b9655182781c5d7f5da59a6cea0f7f5b21b9e0
israel schlesinger <sarelsch@outlook.co.il>
2022-02-09 12:27:06 +0200

    .

--- model_training.py
+++ model_training.py
@@ -2,7 +2,7 @@ from tensorflow_examples.lite.model_maker.core.task.model_spec.image_spec import
 from tflite_model_maker import image_classifier
 from tflite_model_maker.image_classifier import DataLoader
 
-path = r"C:\Users\User\PycharmProjects\ScoreFace\faces-fiq"
+path = "pictures_set"
 data = DataLoader.from_folder(path)
 train_data, rest_data = data.split(0.8)
 validation_data, test_data = rest_data.split(0.5)
